87dd1c8d2402b1bd93898ce4bc7fd6bd970ab2f2
ogu <Ozan.Gunalp@lesfurets.com>
2019-09-11 14:21:42 +0200

    pizza local

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,44 @@
 import "./formik-demo.css";
 import * as React from "react";
 import {render} from "react-dom";
-import {
-  FastField,
-  Field,
-  withFormik,
-  FormikProps
-} from "formik";
+import {Field, FormikProps, withFormik} from "formik";
 
 import Select from "react-select";
 import {DisplayFormikState} from "./formik-helper";
 import * as DOOV from 'doov';
+import {converter, DefaultContext, Function, FunctionMetadata, StringFunction} from 'doov';
+
+export type PizzaSize = 'S' | 'M' | 'L' | 'XL';
+
+export enum PizzaCrust {
+    Neapolitan = 'Neapolitan',
+    NewYork = 'NewYork',
+    StLouis = 'StLouis',
+    Pan = 'Pan',
+    DeepDish = 'DeepDish',
+    Sicilian = 'Sicilian',
+}
+
+export type City = 'Napoli' | 'Paris' | 'Chicago' | 'New York' | 'Hawaii';
+
+export interface PizzaSpec {
+    size?: PizzaSize;
+    crust?: PizzaCrust;
+    toppings?: string[];
+}
+
+export interface PizzaOrder {
+    city?: City;
+    pizza?: PizzaSpec;
+    payment?: 'Credit Card' | 'Cash' | 'Online';
+}
 
 interface FormValues {
     email: string;
-    topics: string[];
-    company: Item;
-    function: string;
+    toppings: string[];
+    city: City;
+    size: PizzaSize;
+    crust: PizzaCrust;
 }
 
 type Item = {
@@ -24,208 +46,285 @@ type Item = {
     label: string;
 };
 
-const options: Item[] = [
-    {value: "Food", label: "Food"},
-    {value: "Being Fabulous", label: "Being Fabulous"},
-    {value: "Ken Wheeler", label: "Ken Wheeler"},
-    {value: "ReasonML", label: "ReasonML"},
-    {value: "Unicorns", label: "Unicorns"},
-    {value: "Kittens", label: "Kittens"}
-  ];
-  
-  const companies: Item[]  = [
-    {value: "facebook", label: "Facebook"},
-    {value: "github", label: "GitHub"}
-  ];
-  
-  const emptyOption = {value: "", label: ""};
-  
+const toppingOptions: Item[] = [
+    {value: "cream_sauce", label: "Cream Sauce"},
+    {value: "tomato_sauce", label: "Tomato Sauce"},
+    {value: "mozzarella", label: "Mozzarella"},
+    {value: "mushrooms", label: "Mushrooms"},
+    {value: "pepperoni", label: "Pepperoni"},
+    {value: "olives", label: "Olives"},
+    {value: "pineapple", label: "Pineapple"},
+];
+
+const cities: Item[] = [
+    {value: "napoli", label: "Napoli"},
+    {value: "paris", label: "Paris"},
+    {value: "new york", label: "New York"},
+    {value: "chicago", label: "Chicago"},
+    {value: "hawaii", label: "Hawaii"}
+];
+
+const pizzaSizeOptions: Item[] = [
+    {value: 'S', label: "Small"},
+    {value: 'M', label: "Medium"},
+    {value: 'L', label: "Large"},
+    {value: 'XL', label: "XLarge"},
+];
+
+const emptyOption: Item = {value: "", label: ""};
+
 
 const email = DOOV.string(DOOV.field('email'));
-const topics = DOOV.iterable(DOOV.field<object, string[]>('topics'));
-const company = DOOV.f(DOOV.field<object, Item>('company'));
+const toppings = DOOV.iterable(DOOV.field<object, Item[]>('toppings'));
+const city = DOOV.f(DOOV.field<object, Item>('city'));
+const size = DOOV.string(DOOV.field<object, string>('size'));
+const optionsValue = new Function<Item[]>(new FunctionMetadata('options value'), (obj: object, ctx?: any) => {
+    return ctx!.props['options'];
+}, (obj: object, value: Item[], ctx?) => {
+    return (ctx!.props['options'] as unknown as Item[]) = value;
+});
+const itemValues = Function.contextual(new FunctionMetadata('item field values'), (obj, ctx) => {
+    return ctx!.props['value'] as unknown as Item[];
+});
+const singleItemValue = Function.contextual(new FunctionMetadata('item field value'), (obj, ctx) => {
+    return ctx!.props['value'] as unknown as Item;
+});
 
+const emailNotNull = DOOV.when(email.isNullOrUndefined().not()).validate();
+const cityNotNull = DOOV.when(city.notEq(emptyOption)).validate();
 const emailValid = DOOV.when(email
-    .mapTo(DOOV.BooleanFunction, (v: string) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(v))
-    .and(email.isNotNull())).validate();
+    .mapTo(DOOV.BooleanFunction, (v: string) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(v))).validate();
 
 const formikEnhancer = withFormik({
-  mapPropsToValues: props => ({
-    email: "",
-    topics: [],
-    company: emptyOption
-  }),
-  validate: values => {
-    let errors = {} as any;
-    const result = emailValid.execute(values);
-    if (!result.value) {
-        errors.email = 'Invalid email address';
-    }
-    return errors;
-  },
-  handleSubmit: (values, {setSubmitting}) => {
-    const payload = {
-      ...values,
-      company: values.company.value,
-      topics: values.topics.map((t:Item) => t.value)
-    };
-    setTimeout(() => {
-      alert(JSON.stringify(payload, null, 2));
-      setSubmitting(false);
-    }, 1000);
-  },
-  displayName: "MyForm"
+    mapPropsToValues: props => ({
+        toppings: [],
+        size: '',
+        crust: emptyOption,
+        city: emptyOption,
+        email: undefined,
+    }),
+    validate: (values) => {
+        let errors: any = {};
+        if (!cityNotNull.execute(values).value) {
+            errors.city = 'Required';
+        }
+        if (!emailNotNull.execute(values).value) {
+            errors.email = 'Required';
+        } else if (!emailValid.execute(values).value) {
+            errors.email = 'Invalid email address';
+        }
+        return errors;
+    },
+    handleSubmit: (values, {setSubmitting}) => {
+        const payload = {
+            ...values,
+            city: values.city.value,
+            topics: values.topics.map((t: Item) => t.value)
+        };
+        setTimeout(() => {
+            alert(JSON.stringify(payload, null, 2));
+            setSubmitting(false);
+        }, 1000);
+    },
+    displayName: "MyForm"
 });
 
 const MyForm: React.SFC<FormikProps<FormValues>> = props => {
     const {
-      values,
-      setValues,
-      touched,
-      dirty,
-      errors,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      handleReset,
-      setFieldValue,
-      setFieldTouched,
-      isSubmitting
+        values,
+        setValues,
+        touched,
+        dirty,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        handleReset,
+        setFieldValue,
+        isSubmitting
     } = props;
 
-    const notEmpty = DOOV.when(company.notEq(emptyOption)).validate();
+    const cityNotEmpty = DOOV.when(city.notEq(emptyOption)).validate();
+
+    const toppingsChangeRule = DOOV.mappings(
+        DOOV.when(city.mapTo(StringFunction, v => v!.value).notEq('hawaii'))
+            .then(DOOV.map(itemValues).to(toppings))
+            .otherwise(DOOV.map(itemValues.mapTo(Function, items => items!.filter(v => v.value !== 'pineapple')))
+                .to(optionsValue))
+    );
+
+    const sizeChangeRule = DOOV.mappings(
+        DOOV.map(singleItemValue).using(converter((obj, input, context) => {
+            let v = input.get(obj, context);
+            return v ? v.value : v;
+        })).to(size),
+    );
+
+    const toppingsOptionsRule = DOOV.when(city.mapTo(StringFunction, v => v!.value).notEq('hawaii'))
+        .then(DOOV.map(toppingOptions).to(optionsValue))
+        .otherwise(DOOV.map(toppingOptions.filter(v => v.value !== 'pineapple')).to(optionsValue));
+
+    const crustOptionsRule = DOOV.map(Object.keys(PizzaCrust).map(v => ({
+        value: v,
+        label: v
+    }) as Item)).to(optionsValue);
 
     return (
         <form onSubmit={handleSubmit}>
-          <label htmlFor="email" style={{display: "block"}}>
-            Email
-          </label>
-          <FastField
-              name="email"
-              component="input"
-              placeholder="Enter your email"
-              type="email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-          />
-          {errors.email && touched.email && (
-              <div style={{color: "red", marginTop: ".5rem"}}>{errors.email}</div>
-          )}
-          <label htmlFor="company" style={{display: "block"}}>
-            Company
-          </label>
-          <Field
-              name="company"
-              component={Select}
-              options={companies.filter(v => v.value.startsWith('git'))}
-              value={values.company}
-              onChange={(value: string) => setFieldValue("company", value)}
-              onBlur={() => setFieldTouched("company")}
-              error={errors.company}
-              touched={touched.company}
-          />
-          <label htmlFor="function" style={{display: "block"}}>
-            Function
-          </label>
-          <Field
-              name="function"
-              component="input"
-              disabled={true}
-              value={values.company.value === 'github' ? 'Developer' : 'Manager'}
-              onChange={(value: string) => setFieldValue("function", value)}
-              onBlur={() => setFieldTouched("function")}
-              error={errors.function}
-              touched={touched.function}
-          />
-          {errors.company && touched.company && (
-              <div style={{color: "red", marginTop: ".5rem"}}>{Object.values(errors.company)}</div>
-          )}
-          <label htmlFor="topics">
-            Topics (select at least 3)
-          </label>
-          <StatefulField
-              name="topics"
-              component={Select}
-              isVisible={notEmpty.execute(values).value}
-              isDisabled={!values.company.value}
-              options={
-                values.company.value ? options : []
-              }
-              isMulti
-              value={values.topics}
-              onChange={(field: string, value: string[]) => {
-                  console.log(props);
-                  setValues(
-                      DOOV.mappings(
-                          DOOV.map(value).to(topics),
-                          DOOV.when(email.length().notEq(0)).then(
-                              DOOV.map(email.concat('.tr')).to(email)
-                              ),
-                          DOOV.map(emptyOption).to(company),
-                          ).execute(values));
-              }}
-              onBlur={(field: string, blur: boolean) => {
-                setFieldTouched(field)
-              }}
-              error={errors.topics}
-          />
-          <button
-              type="button"
-              className="outline"
-              onClick={handleReset}
-              disabled={!dirty || isSubmitting}
-          >
-            Reset
-          </button>
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
+            <StatefulField
+                name="city"
+                label="City"
+                component={Select}
+                options={cities}
+                onChange={(value: string) => setFieldValue("city", value)}
+                value={values.city}
+                onBlur={handleBlur}
+                error={errors.city}
+                touched={touched.city}
+            />
+            <StatefulField
+                label="Size"
+                name="size"
+                component={Select}
+                formValues={values}
+                setFormValues={setValues}
+                visibilityRule={cityNotEmpty}
+                options={pizzaSizeOptions}
+                value={pizzaSizeOptions.find(v => v.value === values.size)}
+                changeRule={sizeChangeRule}
+                onBlur={handleBlur}
+                error={errors.size}
+                touched={touched.size}
+            />
+            <StatefulField
+                name="toppings"
+                label="Toppings"
+                isMulti
+                component={Select}
+                formValues={values}
+                setFormValues={setValues}
+                visibilityRule={cityNotEmpty}
+                optionsRule={toppingsOptionsRule}
+                value={values.toppings}
+                changeRule={toppingsChangeRule}
+                onBlur={handleBlur}
+                error={errors.toppings}
+            />
+            <StatefulField
+                name="crust"
+                label="Crust"
+                component={Select}
+                setFormValues={setValues}
+                formValues={values}
+                visibilityRule={cityNotEmpty}
+                optionsRule={crustOptionsRule}
+                // options={toppingOptions}
+                value={Object.keys(PizzaCrust).find(v => v === values.crust)}
+                // changeRule={toppingsChangeRule}
+                onChange={(value: Item) => setFieldValue('crust', value)}
+                onBlur={handleBlur}
+                error={errors.crust}
+            />
+            <StatefulField
+                name="email"
+                label="Email"
+                component="input"
+                placeholder="Enter your email"
+                type="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+            />
+            {errors.email && touched.email && (
+                <div style={{color: "red", marginTop: ".5rem"}}>{errors.email}</div>
+            )}
+            <button
+                type="button"
+                className="outline"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+            >
+                Reset
+            </button>
+            <button type="submit" disabled={isSubmitting}>
+                🍕
+            </button>
 
-          <DisplayFormikState {...props} />
+            <DisplayFormikState {...props} />
         </form>
     );
-}
+};
 
 class StatefulField extends React.Component<any> {
-  static defaultProps = {
-    isDisabled: false
-  };
+    static defaultProps = {
+        isDisabled: false,
+        isVisible: true,
+        options: undefined,
+    };
 
-  handleChange = (value: string) => {
-    // this is going to call setFieldValue and manually update values[this.props.name]
-    this.props.onChange(this.props.name, value);
-  };
+    constructor(props: any) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleOptions = this.handleOptions.bind(this);
+        this.handleVisibility = this.handleVisibility.bind(this);
+    }
 
-  handleBlur = () => {
-    // this is going to call setFieldTouched and manually update touched[this.props.name]
-    this.props.onBlur(this.props.name, true);
-  };
+    handleOptions() {
+        if (this.props.optionsRule) {
+            const ctx = new DefaultContext();
+            this.props.optionsRule.execute(this.props.formValues, ctx);
+            return ctx.props['options'];
+        } else {
+            return this.props.options;
+        }
+    }
 
-  render() {
-    return (
-        this.props.isVisible &&
-        (<React.Fragment>
-          <Field
-              {...this.props}
-              onChange={this.handleChange}
-              onBlur={this.handleBlur}
-          />
-          {!!this.props.error && this.props.touched && (
-              <div style={{color: "red", marginTop: ".5rem"}}>
-                {this.props.error}
-              </div>
-          )}
-        </React.Fragment>)
-    );
-  }
+    handleVisibility() {
+        if (this.props.visibilityRule) {
+            const ctx = new DefaultContext();
+            return this.props.visibilityRule.execute(this.props.formValues, ctx).value;
+        } else {
+            return this.props.isVisible;
+        }
+    }
+
+    handleChange(value: any) {
+        if (this.props.changeRule) {
+            const ctx = new DefaultContext();
+            ctx.props['value'] = value;
+            this.props.setFormValues(this.props.changeRule.execute(this.props.formValues, ctx));
+        } else {
+            this.props.onChange(value);
+        }
+    };
+
+    render() {
+        return (
+            this.handleVisibility() &&
+            (<React.Fragment>
+                <label htmlFor="function" style={{display: "block", margin: ".5rem"}}>
+                    {this.props.label ? this.props.label : this.props.name}
+                </label>
+                <Field
+                    {...this.props}
+                    onChange={this.handleChange}
+                    options={this.handleOptions()}
+                />
+                {!!this.props.error && this.props.touched && (
+                    <div style={{color: "red", marginTop: ".5rem"}}>
+                        {this.props.error}
+                    </div>
+                )}
+            </React.Fragment>)
+        );
+    }
 }
 
 const MyEnhancedForm = formikEnhancer(MyForm);
 
 const App = () => (
     <div className="app">
-      <MyEnhancedForm/>
+        <MyEnhancedForm/>
     </div>
 );
 
